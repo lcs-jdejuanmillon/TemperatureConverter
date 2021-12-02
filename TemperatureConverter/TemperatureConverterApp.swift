4c7919c97d543c9920ca9b0a75c2d8dd5db233ef
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-12-02 09:56:08 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Jacobo de Juan Millon on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
